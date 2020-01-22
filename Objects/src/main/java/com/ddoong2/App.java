4b134b8e23530f1c032c9e560d82a63b9eed2fdd
daejoon <kkode1911@gmail.com>
2020-01-22 16:31:11 +0900

    Add Objects

---
+++ Objects/src/main/java/com/ddoong2/App.java
@@ -0,0 +1,8 @@
+package com.ddoong2;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Objects");
+    }
+}
---
+++ Objects/src/test/java/com/ddoong2/AppTest.java
@@ -0,0 +1,11 @@
+package com.ddoong2;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void nothing() {
+
+    }
+}
