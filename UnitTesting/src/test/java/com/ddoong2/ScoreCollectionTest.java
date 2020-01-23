8debdf92530932c89958d4e6b669e22efbaaa81d
daejoon <kkode1911@gmail.com>
2020-01-23 18:59:37 +0900

    1.3

--- UnitTesting/src/test/java/com/ddoong2/ScoreCollectionTest.java
+++ UnitTesting/src/test/java/com/ddoong2/ScoreCollectionTest.java
@@ -2,9 +2,22 @@ package com.ddoong2;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
 public class ScoreCollectionTest {
 
     @Test
-    public void test() {
+    public void answersArithmeticMeanOfTowNumbers() {
+        // 준비
+        ScoreCollection collection = new ScoreCollection();
+        collection.add(() -> 5);
+        collection.add(() -> 7);
+
+        // 실행
+        int actualResult = collection.arithmeticMean();
+
+        // 단언
+        assertThat(actualResult, equalTo(6));
     }
 }
