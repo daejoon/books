1479767090f508dc928fa5657b22f91bfbe125bd
daejoon <kkode1911@gmail.com>
2020-01-23 18:56:15 +0900

    1.2.3

--- UnitTesting/src/test/java/com/ddoong2/ScoreCollectionTest.java
+++ UnitTesting/src/test/java/com/ddoong2/ScoreCollectionTest.java
@@ -2,12 +2,9 @@ package com.ddoong2;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
-
 public class ScoreCollectionTest {
 
     @Test
     public void test() {
-        fail("Not yet implemented");
     }
 }
